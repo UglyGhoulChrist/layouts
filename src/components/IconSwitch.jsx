8222c8d90dad0fbefc688a3067a4b23ad50e31d3
UglyGhoulChrist <UglyGhoulChrist@gmail.com>
2023-01-21 13:58:05 +0300

    react-fragment => div

--- src/components/IconSwitch.jsx
+++ src/components/IconSwitch.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { FaThList, FaTh } from "react-icons/fa";
 function IconSwitch({ icon, onSwitch }) {
   return (
-    <>
+    <div>
       {icon === "view_list" && (
         <FaThList onClick={onSwitch} className="icon-switch" />
       )}
       {icon === "view_module" && (
         <FaTh onClick={onSwitch} className="icon-switch" />
       )}
-    </>
+    </div>
   );
 }
 export default IconSwitch;
